14ddb33c6acefc390851dbd309137b36a6727f15
John Lo <johnlo.dev@gmail.com>
2023-04-18 02:45:00 +0800

    Add navigation for banner.

--- lib/ui/pages/home/widgets/home_banner.dart
+++ lib/ui/pages/home/widgets/home_banner.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../constants.dart';
 import '../../../../model/product.dart';
+import '../../detail/detail_page.dart';
 
 class MainBanner extends StatelessWidget {
-  const MainBanner({super.key, required List<Product> bannerList}) : _bannerList = bannerList;
+  const MainBanner({super.key, required List<Product> bannerList})
+      : _bannerList = bannerList;
 
   final List<Product> _bannerList;
 
@@ -17,20 +18,28 @@ class MainBanner extends StatelessWidget {
             scrollDirection: Axis.horizontal,
             itemCount: _bannerList.length,
             itemBuilder: (BuildContext context, int position) {
-              return getBannerItem(position);
+              return getBannerItem(position, context);
             }));
   }
 
-  Widget getBannerItem(int position) {
+  Widget getBannerItem(int position, BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(Dimens.paddingHalf),
         child: SizedBox(
             width: Dimens.widthMainBannerImage,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(Dimens.roundingGeneral),
-                child: Image.network(
-                  _bannerList[position].mainImage,
-                  fit: BoxFit.cover,
-                ))));
+            child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              DetailPage(product: _bannerList[position])));
+                }, // Image tapped
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(Dimens.roundingGeneral),
+                    child: Image.network(
+                      _bannerList[position].mainImage,
+                      fit: BoxFit.cover,
+                    )))));
   }
 }
